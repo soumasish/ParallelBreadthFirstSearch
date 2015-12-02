18517f65e886c93f7aabae57b60e90802685d9e1
Soumasish Goswami <soumasish@gmail.com>
2015-12-02 13:24:05 -0500

    Added driver code to construct edges array

--- src/main.cu
+++ src/main.cu
@@ -4,9 +4,21 @@ int main(int argc, char** argv){
 	
 	const int NUM_VERTICES = 1000;
 	const int NUM_EDGES = 1000;
+	bool createEdgeArray = false;
 
 	Vertex vertices[NUM_VERTICES];
 	Edge edges[NUM_EDGES];
+	
+	if(createEdgeArray == false){
+		for (int i = 0; i < NUM_VERTICES; ++i)
+		{
+			Edge* e = malloc(sizeof(Edge));
+			e->first = (rand() % (NUM_VERTICES+1));
+			e->first = (rand() % (NUM_VERTICES+1));
+			memcpy(edges[i], e, sizeof(e));
+		}
+		createEdgeArray = true;
+	}
 	
 	
 	bool* h_done;
